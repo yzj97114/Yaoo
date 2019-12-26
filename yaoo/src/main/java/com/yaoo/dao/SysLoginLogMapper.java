c72ba206cb1c8e674094361a19c26750985d5df6
yyyzj <792826371@qq.com>
2019-12-27 00:57:45 +0800

    集成Druid

---
+++ yaoo/src/main/java/com/yaoo/dao/SysLoginLogMapper.java
@@ -0,0 +1,17 @@
+package com.yaoo.dao;
+
+import com.yaoo.entity.SysLoginLog;
+
+public interface SysLoginLogMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(SysLoginLog record);
+
+    int insertSelective(SysLoginLog record);
+
+    SysLoginLog selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(SysLoginLog record);
+
+    int updateByPrimaryKey(SysLoginLog record);
+}
