ead4bdbe52bc24bbb3d27a804ec1e8ff73414d0f
yyyzj <792826371@qq.com>
2019-12-26 21:43:15 +0800

    集成Swagger，Mybatis

---
+++ yaoo/src/main/java/com/yaoo/YaooApplication.java
@@ -0,0 +1,21 @@
+package com.yaoo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @Package com.yaoo
+ * @author yyyzj
+ * @date 2019/12/26 21:26
+ * @version V1.0
+ * @Copyright 792826371@qq.com
+ */
+@SpringBootApplication
+@MapperScan("com.yaoo.mapper")
+public class YaooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YaooApplication.class, args);
+    }
+
+}
